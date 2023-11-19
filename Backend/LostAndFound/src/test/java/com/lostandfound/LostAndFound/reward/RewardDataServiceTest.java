3ec88118f48c29fbcfd8debbe119b237ce5fe741
Aman Harishkumar Desai <am839007@dal.ca>
2023-11-19 17:23:35 -0400

    added: tests for getting reward data by Id

--- Backend/LostAndFound/src/test/java/com/lostandfound/LostAndFound/reward/RewardDataServiceTest.java
+++ Backend/LostAndFound/src/test/java/com/lostandfound/LostAndFound/reward/RewardDataServiceTest.java
@@ -2,6 +2,7 @@ package com.lostandfound.LostAndFound.reward;
 
 import static org.mockito.Mockito.when;
 
+import com.lostandfound.LostAndFound.core.exception.LostAndFoundException;
 import com.lostandfound.LostAndFound.reward.entities.RewardData;
 import com.lostandfound.LostAndFound.reward.repo.RewardDataRepository;
 import com.lostandfound.LostAndFound.reward.service.Impl.RewardDataServiceImpl;
@@ -12,6 +13,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class RewardDataServiceTest {
@@ -35,5 +38,36 @@ public class RewardDataServiceTest {
 
     // assert
     Assertions.assertEquals(rewardData, createdRewardData);
+  }
+
+  @Test
+  void testCreateRewardDataException() {
+    // arrange
+    when(rewardDataRepository.save(rewardData)).thenThrow(new LostAndFoundException(""));
+
+    // act
+    // assert
+    Assertions.assertThrows(
+        LostAndFoundException.class, () -> rewardDataService.create(rewardData));
+    // check message
+    Assertions.assertEquals(
+        "Error while creating reward data",
+        Assertions.assertThrows(
+                LostAndFoundException.class, () -> rewardDataService.create(rewardData))
+            .getMessage());
+  }
+
+  @Test
+  void testFindRewardDataById() {
+    // arrange
+    when(rewardDataRepository.findById("123")).thenReturn(Optional.ofNullable(rewardData));
+
+    // act
+    RewardData foundRewardData = rewardDataService.findById("123");
+
+    // assert
+    Assertions.assertEquals(rewardData.getId(), foundRewardData.getId());
+    Assertions.assertEquals(rewardData.getTitle(), foundRewardData.getTitle());
+    Assertions.assertEquals(rewardData.getDescription(), foundRewardData.getDescription());
   }
 }
