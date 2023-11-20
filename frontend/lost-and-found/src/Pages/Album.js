e909e476efb8c1305ea56406dac96f2519111550
AICad12 <hr699843@dal.ca>
2023-11-20 16:45:41 -0400

    feat: pagination added for item posted page and approve filter added

--- frontend/lost-and-found/src/Pages/Album.js
+++ frontend/lost-and-found/src/Pages/Album.js
@@ -13,6 +13,14 @@ import sensitiveImg from "../Assets/Images/sensitive.jpg";
 import Button from "@mui/material/Button";
 import DashboardOptions from "../constants/DashboardOptions";
 import { ApiRequest } from "../helpers/api-request";
+import {
+  Form,
+  Modal,
+  Row,
+  Col,
+  Image
+} from "react-bootstrap";
+import { BsImage } from "react-icons/bs";
 
 const Album = (props) => {
   const [revokeRequest, setRevokeRequest] = useState({
@@ -25,7 +33,11 @@ const Album = (props) => {
   const [currentLoggedinUser, setcurrentLoggedinUser] = useState(
     localStorage.getItem("user_email")
   );
+
+  const [linkedLostItem, setLinkedLostItem] = useState(null);
+  const [currentSelectedItemID, setcurrentSelectedItemID] = useState(null);
   const [filterClaimStatus, setfilterClaimStatus] = useState(1);
+  const [showModal, setShowModal] = useState(false);
 
   const cardStyle = {
     height: "100%",
@@ -41,6 +53,11 @@ const Album = (props) => {
     },
   };
 
+  const gridItemStyle = {
+    flex: 1, // Allow the Grid item to grow and shrink, but don't let it shrink below its content's intrinsic size
+    // maxWidth: '300px', // Adjust the maximum width as needed
+  };
+
   const noDataStyler = {
     display: "flex",
     justifyContent: "center",
@@ -51,7 +68,7 @@ const Album = (props) => {
       // Example filters
     },
     page: 0,
-    size: 10,
+    size: 2,
     sortField: "postedAt",
     sortDirection: "DESC", // or 'DESC' depending on your requirement
   });
@@ -59,16 +76,25 @@ const Album = (props) => {
   console.log("value:", props.value);
 
   useEffect(() => {
+    setCurrentPage(1);
+    setTotalPages(1);
+    
     const fetchData = async () => {
-      await getResult(props, setItems, searchFilter);
+      await getResult();
     };
 
     fetchData();
-  }, [props.value, props.filterParams, searchFilter]);
+  }, [props.value, props.filterParams]);
 
   const [items, setItems] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // useEffect(() => {
+  //   // This will run whenever currentPage changes
+  //   console.log("currentPage changed:", currentPage);
+  //   getResult(currentPage); // Assuming getResult is defined in your component
+  // }, [currentPage]);
 
   const addFilter = (attributeName, value, mode) => {
     setSearchFilter((prevFilter) => ({
@@ -97,7 +123,7 @@ const Album = (props) => {
       });
       // console.log("GET request successful:", response.content);
       // setItems(response.content);
-      await getResult(props, setItems, searchFilter);
+      await getResult();
 
       // });
       // const response = await axios.put(
@@ -114,8 +140,9 @@ const Album = (props) => {
     // fetchData();
   };
 
-  const handleApprove = async (itemId, claimRequestUserId) => {
-    console.log("ItemID clicked", itemId);
+  const handleAccept = async (itemId, claimRequestUserId) => {
+    console.log("ItemID clicked", itemId, claimRequestUserId);
+    console.log("selectedItem", currentSelectedItemID);
     // const fetchData = async () => {
 
     // setRevokeRequest((prevData) => ({
@@ -136,9 +163,10 @@ const Album = (props) => {
       // );
       await ApiRequest.fetch({
         method: "put",
-        url: `${API_URL}/api/v1/items/claims/accept?itemId=${itemId}&userId=${revokeRequest.userId}&claimRequestUserId=${claimRequestUserId}`,
+        url: `${API_URL}/api/v1/items/claims/accept?itemId=${currentSelectedItemID}&userId=${revokeRequest.userId}&claimRequestLostItemId=${itemId}`,
       });
-      await getResult(props, setItems, searchFilter);
+      await getResult();
+      setShowModal(false);
       // console.log("GET request successful:", response.data.content);
       // setItems(response.data.content);
     } catch (error) {
@@ -164,10 +192,10 @@ const Album = (props) => {
     const selectedArray = getValueArray();
 
     return selectedArray ? (
-      <Grid container spacing={4}>
-        {selectedArray.map((mail, index) => (
-          <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
-            <Card sx={cardStyle}>
+      <React.Fragment>
+        {Object.entries(selectedArray).map(([key, value]) => (
+          <Grid item key={key} xs={12} sm={6} md={4} lg={3}>
+            <Card key={key} sx={cardStyle}>
               <CardMedia
                 component="div"
                 sx={{
@@ -184,12 +212,12 @@ const Album = (props) => {
                 </Typography>
                 <br />
                 <Typography style={{ wordWrap: "break-word" }} gutterBottom>
-                  User: {mail}
+                  User: {value}
                 </Typography>
               </CardContent>
               <CardActions>
                 <Button
-                  onClick={() => handleApprove(item.id, mail)}
+                  onClick={() => handleReviewRequest(key, item.id)}
                   size="large"
                   style={{
                     backgroundColor: "green",
@@ -197,17 +225,17 @@ const Album = (props) => {
                     color: "white",
                   }}
                 >
-                  Approve
+                  Review
                 </Button>
               </CardActions>
             </Card>
           </Grid>
         ))}
-      </Grid>
+      </React.Fragment>
     ) : null;
   };
 
-  async function getResult(props, setItems, searchFilter) {
+  async function getResult(currentPage = 0) {
     const currentLoggedinUser = localStorage.getItem("user_email");
 
     console.log("filter props", props.filterParams);
@@ -215,12 +243,14 @@ const Album = (props) => {
 
     // Assuming `value` is defined somewhere in your component
     if (props.value === 0) {
+      console.log("current Page", currentPage);
       const updatedFilter = {
         ...searchFilter,
+        page: currentPage,
         filters: {
           ...searchFilter.filters,
           foundItem: { value: true, mode: "equals" },
-          createdBy: { value: currentLoggedinUser, mode: "contains" },
+          createdBy: { value: currentLoggedinUser, mode: "equals" },
         },
       };
       if (
@@ -265,6 +295,8 @@ const Album = (props) => {
     } else if (props.value === 1) {
       const updatedFilter = {
         ...searchFilter,
+        page: 0,
+        size: 1000,
         filters: {
           ...searchFilter.filters,
           claimRequested: { value: currentLoggedinUser, mode: "contains" },
@@ -277,9 +309,11 @@ const Album = (props) => {
 
       const updatedFilter = {
         ...searchFilter,
+        page: 0,
+        size: 1000,
         filters: {
           ...searchFilter.filters,
-          createdBy: { value: currentLoggedinUser, mode: "contains" },
+          createdBy: { value: currentLoggedinUser, mode: "equals" },
         },
       };
       getFilteredData(updatedFilter, setItems);
@@ -296,6 +330,7 @@ const Album = (props) => {
         console.log("GET request successful:", response.content);
         setItems(response.content);
         setTotalPages(response.totalPages);
+        setCurrentPage(response.number + 1);
       });
       // const response = await axios.post(
       //   `${API_URL}items/search`,
@@ -308,41 +343,77 @@ const Album = (props) => {
       console.error("Error:", error);
     }
   }
+  const handleCloseModal = () => {
+    setShowModal(false);
+    // setSelectedLostItem(null);
+    // setNewImages([]);
+  };
+  const handleReviewRequest = (key, primeItemID) => {
+    console.log("key", key);
+    setcurrentSelectedItemID(primeItemID);
+    try {
+      ApiRequest.fetch({
+        method: "get",
+        url: `${API_URL}/api/v1/items/`+key,
+      }).then((response) => {
+        console.log("GET item requested successful:", response.content);
+        setLinkedLostItem(response);
+        setShowModal(true);
+
+      });
+      // const response = await axios.post(
+      //   `${API_URL}items/search`,
+      //   updatedFilter,
+      //   { headers }
+      // );
+      // console.log("GET request successful:", response.data.content);
+      // setItems(response.data.content);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    // setSelectedLostItem(null);
+    // setNewImages([]);
+  };
 
   const Pagination = () => {
     // const totalPages = Math.ceil(totalItems / itemsPerPage);
     // const [currentPage, setCurrentPage] = useState(1);
 
     const handleNextPage = () => {
-      setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+      const newPage = Math.min(currentPage + 1, totalPages);
+      console.log("newPage", newPage);
+      setCurrentPage(newPage); //, () => {
+      // });
+      console.log("currentPage", currentPage);
+      getResult(newPage - 1);
     };
 
     const handlePrevPage = () => {
-      setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+      const newPage = Math.max(currentPage - 1, 1);
+      setCurrentPage(newPage, () => {
+        console.log("currentPage", currentPage);
+      });
+      getResult(newPage - 1);
     };
 
     return (
       <div>
         <div>
-          <button onClick={handlePrevPage} disabled={currentPage === 0}>
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>
             Previous
           </button>
           <span>
             {" "}
-            Page {currentPage + 1} of {totalPages+1}{" "}
+            Page {currentPage} of {totalPages}{" "}
           </span>
           <button
             onClick={handleNextPage}
-            disabled={currentPage+1 === totalPages+1}
+            disabled={currentPage === totalPages}
           >
             Next
           </button>
         </div>
         {/* Render your content for the current page */}
-        <div>
-          <h2>Page {currentPage + 1} Content</h2>
-          {/* Your content goes here */}
-        </div>
       </div>
     );
   };
@@ -410,10 +481,199 @@ const Album = (props) => {
         <div style={noDataStyler}>No Data</div>
       )}
 
-      <div>
-        <h1>Pagination Example</h1>
+{props.value === 0 ? (<div>
         <Pagination />
-      </div>
+      </div>) : ""}
+
+      <Modal
+          show={showModal}
+          onHide={handleCloseModal}
+          dialogClassName="custom-modal"
+          size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title style={{ color: "#75e6a3" }}>
+              {"Linked Lost Item"}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="formTitle">
+                <Form.Label
+                  style={{
+                    color: "#333",
+                    marginRight: "5px",
+                    fontWeight: "bold",
+                  }}>
+                  Title
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter title"
+                  value={linkedLostItem?.title || ""}
+                />
+              </Form.Group>
+              <Form.Group controlId="formDescription">
+                <Form.Label
+                  style={{
+                    color: "#333",
+                    marginRight: "5px",
+                    fontWeight: "bold",
+                  }}>
+                  Description
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  placeholder="Enter description"
+                  value={linkedLostItem?.description || ""}
+                />
+              </Form.Group>
+              <Form.Label
+                style={{
+                  color: "#333",
+                  marginRight: "5px",
+                  fontWeight: "bold",
+                }}>
+                Item Category
+              </Form.Label>
+              <Form.Select
+                style={{ width: "100%", height: "40px" }}
+                aria-label="personal"
+                value={linkedLostItem?.category || "personal"}>
+                <option>Select Category</option>
+                <option value="personal">Personal Item</option>
+                <option value="electronics">Electronics</option>
+                <option value="document">Document</option>
+              </Form.Select>
+              <Form.Label
+                style={{
+                  color: "#333",
+                  marginRight: "5px",
+                  fontWeight: "bold",
+                  marginTop: "5px",
+                }}>
+                Images:
+              </Form.Label>
+              <Container>
+                <Row>
+                  {(linkedLostItem?.image || []).map((img, index) => (
+                    <Col
+                      xs={4}
+                      className="text-center p-2 shadow mb-4 item-edit-card"
+                      key={index}>
+                      <div>
+                        <Image
+                          src={img}
+                          alt={`Image ${index + 1}`}
+                          style={{ height: "150px", width: "150px" }}
+                        />
+                      </div>
+                      {/* <Button
+                        className="delete-image-button mt-2"
+                        onClick={() => handleDeleteImage(img)}
+                        variant="danger"
+                        size="sm"
+                        block>
+                        Delete
+                      </Button> */}
+                    </Col>
+                  ))}
+
+                  {/* {newImages?.map((img, index) => (
+                    <Col
+                      xs={4}
+                      className="text-center p-2 shadow mb-4 item-edit-card"
+                      key={index}>
+                      <div>
+                        <Image
+                          src={URL.createObjectURL(newImages[index])}
+                          alt={`Image ${index + 1}`}
+                          style={{ height: "150px", width: "150px" }}
+                        />
+                      </div>
+                      <Button
+                        className="delete-image-button mt-2"
+                        onClick={() => handleDeleteNewImage(index)}
+                        variant="danger"
+                        size="sm"
+                        block>
+                        Delete
+                      </Button>
+                    </Col>
+                  ))} */}
+                </Row>
+              </Container>
+              {/* <Form.Group controlId="formImages">
+                    
+                            <Form.Control
+                                style={{ marginTop: '10px' }}
+                                type="file"
+                                accept="image/*"
+                                multiple
+                                onChange={handleNewImagesChange}
+                                className="lost-item-input"
+                            />
+                    </Form.Group> */}
+
+              {/* <Row className="mb-3 align-items-center">
+                <Col xs={6} md={4} className="d-flex">
+                  <Form.Label style={{ fontWeight: "bold" }}>
+                    Add New Images:
+                  </Form.Label>
+                </Col>
+                <Col xs={6} md={8} className="d-flex align-items-center">
+                  <label
+                    htmlFor="fileInput"
+                    style={{
+                      cursor: "pointer",
+                      color: "#007bff",
+                      display: "flex",
+                      alignItems: "center",
+                    }}>
+                    <BsImage style={{ marginRight: "5px" }} />
+                    Choose Images
+                  </label>
+                </Col>
+              </Row> */}
+
+              {/* <div className="lost-item-group  mt-3">
+                <Form.Label style={{ color: "#333", fontWeight: "bold" }}>
+                  Location Picker
+                </Form.Label>
+                <MapWrapper
+                  locations={
+                    locations.length > 0
+                      ? locations
+                      : [
+                          {
+                            lat: editedLostItem?.location?.y,
+                            lng: editedLostItem?.location?.x,
+                          },
+                        ]
+                  }
+                  setLocationsFun={setLocations}
+                  isEdit={true}
+                />
+              </div> */}
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+          <Button variant="contained" onClick={() => handleAccept(linkedLostItem.id, linkedLostItem.createdBy)} color="success">
+              Accept
+            </Button>
+            <Button variant="contained" color="error">
+              Reject
+            </Button>
+            <Button variant="secondary">
+              Close
+            </Button>
+            <Button
+              variant="primary"
+              // onClick={handleSaveChanges}
+              className="save-color-button">
+              {/* {selectedLostItem ? "Save Changes" : "Confirm"} */}
+            </Button>
+          </Modal.Footer>
+        </Modal>
     </main>
 
     // </ThemeProvider>
